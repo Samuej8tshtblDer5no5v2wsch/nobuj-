2248557e82b8035c96915a32ebf348efd8856325
Samuej8tshtblDer5no5v2wsch <zhuangba759421@126.com>
2025-03-26 09:00:00 +0000

    Implement basic Java Email Service

---
+++ services/EmailService.java
@@ -0,0 +1,1 @@
+package com.example.services;\n\npublic class EmailService {\n    public void sendEmail(String to, String subject, String body) {\n        System.out.println("Sending email to " + to);\n    }\n}
